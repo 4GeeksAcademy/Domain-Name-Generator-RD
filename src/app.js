050f412f741acf8765504801dbc40e14c2f054e6
Rudy Ducatel <109167509+RDXXIV@users.noreply.github.com>
2025-05-29 00:32:45 +0000

    added files to the repo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let end = ['.com', '.net'];
+  
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++){
+      for (let n = 0; n < noun.length; n++){
+        for (let e = 0; e < end.length; e++){
+          let result = pronoun[p] + adj[a] + noun[n] + end[e];
+          console.log(result);
+          document.getElementById("output").innerHTML += result + "<br>"
+        }
+      }
+    }
+  }
+  
 };
